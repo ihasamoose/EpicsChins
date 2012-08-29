cdbbb7a771485f7fc094d9b20cdc3b96eb154cbc
Austin Taylor <epics@2c1c.net>
2012-08-28 21:05:46 -0600

    Made project object oriented

---
+++ EpicsChins/util/Tiles.java
@@ -0,0 +1,40 @@
+package EpicsChins.util;
+
+import org.powerbot.game.api.wrappers.Area;
+import org.powerbot.game.api.wrappers.Tile;
+
+import java.util.Arrays;
+
+public class Tiles {
+	public final static Tile[] PATH_TO_PRAYER_FROM_GE = {new Tile(3185, 3492, 0), new Tile(3197, 3480, 0), new Tile(3196, 3462, 0), new Tile(3216, 3464, 0), new Tile(3233, 3466, 0), new Tile(3245, 3480, 0)};
+	public final static Tile[] PATH_TO_PRAYER_FROM_TELE = {new Tile(3216, 3464, 0), new Tile(3233, 3466, 0), new Tile(3245, 3480, 0)};
+	public final static Tile[] PATH_TO_CHIN_TILE_1 = {new Tile(2770, 9120, 0), new Tile(2758, 9133, 0), new Tile(2740, 9137, 0), new Tile(2722, 9140, 0), new Tile(2714, 9138, 0)};
+	public final static Tile[] PATH_TO_CHIN_TILE_2 = {new Tile(2731, 9130, 0), new Tile(2744, 9125, 0)};
+	public final static Tile[] PATH_TO_CHIN_TILE_3 = {new Tile(2743, 9120, 0), new Tile(2738, 9120, 0), new Tile(2737, 9121, 0), new Tile(2720, 9118, 0)};
+	public final static Tile[] PATH_TO_BANK_TILE = {new Tile(3205, 3440, 0), new Tile(3191, 3451, 0), new Tile(3175, 3459, 0)};
+	public final static Tile TILE_GRAND_BANK = new Tile(3181, 3502, 0);
+	public final static Tile TILE_GRAND_TREE = new Tile(3185, 3508, 0);
+	public final static Tile TILE_APE_LADDER_TOP = new Tile(2764, 2703, 0);
+	public final static Tile TILE_CHIN_1 = new Tile(2715, 9127, 0);
+	public final static Tile TILE_CHIN_2 = new Tile(2746, 9122, 0);
+	public final static Tile TILE_CHIN_3 = new Tile(2709, 9116, 0);
+	public final static Tile TILE_CHIN_4 = new Tile(2701, 9111, 0);
+	public final static Tile TILE_TREE_DOOR = new Tile(2466, 3491, 0);
+	public final static Tile TILE_TREE_DAERO = new Tile(2480, 3488, 1);
+	public final static Tile TILE_PRAYER = new Tile(3253, 3485, 0);
+	public final static Tile TILE_INSIDE_TREE_DOOR = new Tile(2466, 3493, 0);
+
+	public final static java.util.List<Tile> CHIN_LIST = Arrays.asList(Tiles.TILE_CHIN_1, Tiles.TILE_CHIN_2, Tiles.TILE_CHIN_3, Tiles.TILE_CHIN_4);
+
+	public final static Area AREA_CHIN_3_4 = new Area(new Tile(2709, 9116, 0), new Tile(2701, 9111, 0));
+	public final static Area AREA_GE = new Area(new Tile(3135, 3464, 0), new Tile(3203, 3516, 0));
+	public final static Area AREA_BLINDFOND_ZONE = new Area(new Tile(2660, 4501, 0), new Tile(2641, 4531, 0));
+	public final static Area AREA_CRASH_ISLAND = new Area(new Tile(2880, 2735, 0), new Tile(2903, 2711, 0));
+	public final static Area AREA_GRAND_TELE = new Area(new Tile(3208, 3430, 0), new Tile(3217, 2422, 0));
+	public final static Area AREA_SPIRIT_MID = new Area(new Tile(2544, 3172, 0), new Tile(2541, 3167, 0));
+	public final static Area AREA_GNOME_STRONGHOLD = new Area(new Tile(2470, 3440, 0), new Tile(2457, 3492, 0));
+	public final static Area AREA_APE_ATOLL = new Area(new Tile(2809, 2690, 0), new Tile(2753, 2718, 0));
+	public final static Area AREA_GNOME_LEVEL_ONE = new Area(new Tile(2490, 3478, 1), new Tile(2440, 3512, 1));
+	public final static Area AREA_APE_ATOLL_DUNGEON = new Area(new Tile(2805, 9144, 0), new Tile(2704, 9043, 0));
+
+}
---
+++ EpicsChins/util/tasks/Banking.java
@@ -0,0 +1,223 @@
+package EpicsChins.util.tasks;
+
+import EpicsChins.util.Data;
+import EpicsChins.util.GUI;
+import EpicsChins.util.Method;
+import EpicsChins.util.Tiles;
+import org.powerbot.concurrent.strategy.Strategy;
+import org.powerbot.game.api.methods.Calculations;
+import org.powerbot.game.api.methods.Game;
+import org.powerbot.game.api.methods.Walking;
+import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.tab.Equipment;
+import org.powerbot.game.api.methods.tab.Inventory;
+import org.powerbot.game.api.methods.widget.Bank;
+import org.powerbot.game.api.methods.widget.Camera;
+import org.powerbot.game.api.util.Filter;
+import org.powerbot.game.api.util.Random;
+import org.powerbot.game.api.util.Time;
+import org.powerbot.game.api.wrappers.node.Item;
+import org.powerbot.game.bot.Context;
+
+/**
+ * User: Epics
+ * Date: 8/28/12
+ * Time: 8:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Banking extends Strategy implements Runnable {
+
+	@Override
+	public void run() {
+		Item greegree = Inventory.getItem(Data.GREEGREE_IDS);
+
+		if (Data.logBankingCode == 0) {
+			Context.get().getActiveScript().log.info("Running banking code");
+			Data.logBankingCode++;
+		}
+
+		if (Tiles.AREA_GRAND_TELE.contains(Players.getLocal().getLocation())) {
+			Context.get().getActiveScript().log.info("You have the n00b varrock teleport. Walking to bank");
+		}
+
+		Method.checkRun();
+		if (!Bank.isOpen()) {
+			if (Tiles.TILE_GRAND_BANK != null && Calculations.distanceTo(Tiles.TILE_GRAND_BANK) >= 4) {
+				Walking.findPath(Tiles.TILE_GRAND_BANK).traverse();
+				Camera.turnTo(Tiles.TILE_GRAND_BANK);
+				Method.getSpeedCheck();
+			} else if (Calculations.distanceTo(Tiles.TILE_GRAND_BANK) <= 8) {
+				Bank.open();
+			}
+		}
+		if (Data.chinNumber < 2000 && Bank.isOpen()) {
+			if (Data.chinNumber == 0) {
+				Context.get().getActiveScript().log.info("NO chins detected. This means that you haven't set up your equipment right. Check it and try again!");
+				Context.get().getActiveScript().stop();
+				Game.logout(true);
+			}
+			if (Data.chinNumber < 2000 && Bank.withdraw(10034, 2000)) {
+				Time.sleep(80);
+				Bank.close();
+				if (!Bank.isOpen()) {
+					Context.get().getActiveScript().log.info("Recalculating chin count");
+					Data.runCheck = true;
+					while (Data.runCheck = true) {
+						Time.sleep(5);
+					}
+				}
+			} else if (Bank.getItem(10034).getStackSize() <= 1500 && Data.chinNumber < 2000) {
+				Context.get().getActiveScript().log.info("Not enough chins to continue! Shutting down...");
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+			if (Bank.close()) {
+				if (Inventory.getCount(10034) >= 0) {
+					Item chinItem = Inventory.getItem(10034);
+					chinItem.getWidgetChild().click(true);
+					if (Inventory.getCount(10034) < 1) {
+						Data.chinNumber = Equipment.getItem(10034).getStackSize();
+					}
+				}
+			}
+		}
+
+		if (Data.usingGreegree) {
+			Context.get().getActiveScript().log.info("Selected use a greegree, banking accordingly");
+		} else {
+			Context.get().getActiveScript().log.info("Selected not to use a greegree, banking accordingly");
+		}
+
+		if (Inventory.getCount() == 28 && Inventory.getCount(GUI.foodUser) < 1) {
+			Context.get().getActiveScript().log.info("Supplies are done wrong, restarting...");
+			Bank.depositInventory();
+		}
+		if (Inventory.getCount(GUI.foodUser) == 0 && Bank.getItemCount(GUI.foodUser) >= 1) {
+			Bank.withdraw(GUI.foodUser, 1);
+			Time.sleep(80);
+			if (Inventory.getCount(GUI.foodUser) != 0) {
+				Context.get().getActiveScript().log.info("Food withdrawn");
+			}
+		} else if (Bank.getItem(GUI.foodUser).getStackSize() <= 1 && Inventory.getCount(GUI.foodUser) < 1) {
+			Context.get().getActiveScript().log.severe("Food isn't present, shutting down...");
+			Bank.close();
+			Game.logout(true);
+			Context.get().getActiveScript().stop();
+		}
+		if (greegree == null && Bank.isOpen()) {
+			Item greegreeItem = Bank.getItem(new Filter<Item>() {
+				@Override
+				public boolean accept(final Item m) {
+					for (int id : Data.GREEGREE_IDS) {
+						if (m.getId() == id) {
+							return true;
+						}
+					}
+					return false;
+				}
+			});
+			if (greegreeItem != null && Inventory.getCount(greegreeItem.getId()) == 0 && Data.usingGreegree && Bank.getItemCount(greegreeItem.getId()) >= 1) {
+				Bank.withdraw(greegreeItem.getId(), 1);
+				Time.sleep(80);
+				if (Inventory.getCount(greegreeItem.getId()) != 0) {
+					Context.get().getActiveScript().log.info("Greegree withdrawn");
+				}
+			} else if (Bank.getItemCount(greegreeItem.getId()) == 0 && Data.usingGreegree && Inventory.getCount(greegreeItem.getId()) == 0) {
+				Context.get().getActiveScript().log.info("No greegree is present. Shutting down...");
+				Bank.close();
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+
+		}
+		if (Inventory.getCount(Data.POT_PRAYER_DOSE_4) == 0) {
+			Bank.withdraw(Data.POT_PRAYER_DOSE_4, 18);
+			Time.sleep(80);
+			if (Inventory.getCount(Data.POT_PRAYER_DOSE_4) != 0) {
+				Context.get().getActiveScript().log.info("Prayer pots withdrawn");
+			} else if (Bank.getItem(Data.POT_PRAYER_DOSE_4).getStackSize() < 18 && Inventory.getCount(Data.POT_PRAYER_DOSE_4) < 18) {
+				Context.get().getActiveScript().log.info("Not enough prayer pots. Shutting down... ");
+				Bank.close();
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+		}
+		if (Inventory.getCount(GUI.antipoisonUser) == 0 && Bank.getItemCount(GUI.antipoisonUser) > 0) {
+			Bank.withdraw(GUI.antipoisonUser, 1);
+			Time.sleep(80);
+			if (Inventory.getCount(GUI.antipoisonUser) != 0) {
+				Context.get().getActiveScript().log.info("Antipoison withdrawn");
+			} else if (Bank.getItem(GUI.antipoisonUser).getStackSize() < 1 && Inventory.getCount(GUI.antipoisonUser) < 1) {
+				Context.get().getActiveScript().log.info("Not enough antipoison. Shutting down...");
+				Bank.close();
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+		}
+		if (Inventory.getCount(Data.TAB_VARROCK) == 0 && Bank.getItem(Data.TAB_VARROCK).getStackSize() > 0) {
+			Bank.withdraw(Data.TAB_VARROCK, 1);
+			Time.sleep(80);
+			if (Inventory.getCount(Data.TAB_VARROCK) != 0) {
+				Context.get().getActiveScript().log.info("Varrock tab withdrawn");
+			} else if (Bank.getItem(Data.TAB_VARROCK).getStackSize() == 0 && Inventory.getCount(Data.TAB_VARROCK) < 1) {
+				Context.get().getActiveScript().log.info("Not enough tabs. Shutting down...");
+				Bank.close();
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+		}
+		if (Inventory.getCount(Data.FLASK_PRAYER_RENEWAL_FULL) == 0) {
+			Bank.withdraw(Data.FLASK_PRAYER_RENEWAL_FULL, 3);
+			Time.sleep(80);
+			if (Inventory.getCount(Data.FLASK_PRAYER_RENEWAL_FULL) != 0) {
+				Context.get().getActiveScript().log.info("Renewal flasks withdrawn");
+			} else if (Bank.getItem(Data.FLASK_PRAYER_RENEWAL_FULL).getStackSize() < 3 && Inventory.getCount(Data.FLASK_PRAYER_RENEWAL_FULL) < 3) {
+				Context.get().getActiveScript().log.info("Not enough prayer renewal flasks. Shutting down...");
+				Bank.close();
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+		}
+		if (Inventory.getCount(Data.FLASK_RANGING_FULL) == 0) {
+			Bank.withdraw(Data.FLASK_RANGING_FULL, 3);
+			Time.sleep(80);
+			if (Inventory.getCount(Data.FLASK_RANGING_FULL) != 0) {
+				Context.get().getActiveScript().log.info("Range flasks withdrawn");
+			} else if (Bank.getItem(Data.FLASK_RANGING_FULL).getStackSize() < 3 && Inventory.getCount(Data.FLASK_RANGING_FULL) < 3) {
+				Context.get().getActiveScript().log.info("Not enough ranged flasks. Shutting down...");
+				Bank.close();
+				Game.logout(true);
+				Context.get().getActiveScript().stop();
+			}
+			Bank.close();
+		}
+		if (Players.getLocal().getHpPercent() <= 70) {
+			Context.get().getActiveScript().log.info("HP is low when banking, eating");
+			Bank.open();
+			Time.sleep(Random.nextInt(500, 700));
+			if (Bank.isOpen()) {
+				Bank.depositInventory();
+				Bank.withdraw(GUI.foodUser, 2);
+				Bank.close();
+			}
+			final Item FOOD = Inventory.getItem(GUI.foodUser);
+			if (FOOD.getWidgetChild().interact("Eat")) {
+				Time.sleep(Random.nextInt(900, 1200));
+			}
+		}
+		Data.logBankingCode = 0;
+	}
+
+	@Override
+	public boolean validate() {
+		int antipoisonData = 0;
+		for (Item y : Inventory.getItems()) {
+			for (int x : Data.ANTIPOISON_ALL) {
+				if (y.getId() == x) {
+					antipoisonData++;
+				}
+			}
+		}
+		return (Inventory.getCount(Data.POT_PRAYER) <= 1 && Data.START_SCRIPT && Game.isLoggedIn() && !Data.runCheck || Data.chinNumber <= 100 && Data.START_SCRIPT && Game.isLoggedIn() && !Data.runCheck || Method.isPoisoned() && antipoisonData == 0 && Data.START_SCRIPT && Game.isLoggedIn() && !Data.runCheck || Players.getLocal().getHpPercent() <= 25) && !Data.runCheck && Data.START_SCRIPT && Game.isLoggedIn() || Inventory.getCount(Data.TAB_VARROCK) == 0 && Data.START_SCRIPT && Game.isLoggedIn() && !Data.runCheck;
+	}
+}
